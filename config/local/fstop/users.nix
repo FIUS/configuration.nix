fa64534094b3a3042aec5a832be174acab567da5
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-12-02 16:42:16 +0100

    Change passwords again and make users immutable

--- config/local/fstop/users.nix
+++ config/local/fstop/users.nix
@@ -2,13 +2,13 @@
 
 {
   users = {
-    mutableUsers = true;
+    mutableUsers = false;
     users = {
       groot = {
         isNormalUser = true;
         uid = 1000;
         extraGroups = [ "wheel" "networkmanager" "audio" ];
-        hashedPassword = "$6$FeAbRdIK$b3t7Ecyvq7z9ZLa9pOq4ed94yiXB0inFq/WiJmQlVVPeOkEep5WjpVWBY//pNu6KLnOy.1vPA5Z5kpDApaC8G/";
+        hashedPassword = "$6$vdxj/WF153VDLHxJ$PGcMJN6KnNDaSz2TvFXwhIIyqZkoykmJXi5mFB7x1fxXnsVBEZnKVcni7uZFLS4fqCgPO.hs7MWhEmz.cG.js/";
       };
       fs = {
         isNormalUser = true;
@@ -16,7 +16,7 @@
         password = "fs";
         extraGroups = [ "networkmanager" "audio" ];
       };
-      root.hashedPassword = "$6$FeAbRdIK$b3t7Ecyvq7z9ZLa9pOq4ed94yiXB0inFq/WiJmQlVVPeOkEep5WjpVWBY//pNu6KLnOy.1vPA5Z5kpDApaC8G/";
+      root.hashedPassword = "$6$vdxj/WF153VDLHxJ$PGcMJN6KnNDaSz2TvFXwhIIyqZkoykmJXi5mFB7x1fxXnsVBEZnKVcni7uZFLS4fqCgPO.hs7MWhEmz.cG.js/";
     };
   };
   security.sudo.wheelNeedsPassword = false;
