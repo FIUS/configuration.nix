e8ffb8bb48a12ee5504055f31ea3d321e4732954
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2019-12-02 17:34:58 +0100

    Add MOTD

--- config/local/fstop/shells.nix
+++ config/local/fstop/shells.nix
@@ -16,5 +16,24 @@
     # ctrl+backspace, ctrl+delete
     bind \b backward-kill-word
     bind \e\[3\;5~ kill-word
+    echo "Welcome to NixOS."
+    echo "To install any package you have three posibilities:"
+    echo "If only need the program for a short duration use:"
+    echo "nix-shell -p myProgram"
+    echo "If you need the program permanently but only for the current user use:"
+    echo "nix-env -iA nixos.myProgram"
+    echo "If you want the program globally add it to the configuration on 
+github.com/fius/configuration.nix, and follow the instrcutions there to deploy it."
+  '';
+  
+  environment.interactiveShellInit = ''
+    echo "Welcome to NixOS."
+    echo "To install any package you have three posibilities:"
+    echo "If only need the program for a short duration use:"
+    echo "nix-shell -p myProgram"
+    echo "If you need the program permanently but only for the current user use:"
+    echo "nix-env -iA nixos.myProgram"
+    echo "If you want the program globally add it to the configuration on 
+github.com/fius/configuration.nix, and follow the instrcutions there to deploy it."
   '';
 }
