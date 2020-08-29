4d545d0c717ffbac218aef158564d69bb87f19dd
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-08-29 14:26:42 +0200

    Move prusa slicer to fius nur

--- config/local/fstop/pkgs/default.nix
+++ config/local/fstop/pkgs/default.nix
@@ -58,7 +58,7 @@
     tdesktop
     # Other
     nur.repos.marzipankaiser.drinklist-cli
-    nur.repos.neumantm.prusa-slicer-2_3-alpha
+    nur.repos.fius.prusa-slicer-2_3-alpha
     # Systems tools
     ntfs3g
     file
